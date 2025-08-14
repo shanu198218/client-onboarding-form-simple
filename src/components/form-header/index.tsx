a877f88601c01ed01e41955b3244fdcdb589e7f3
shanu198218 <mgshamalidilrukshi@gmail.com>
2025-08-14 22:18:36 +0530

    feat: add placeholders and remove unused color codes

--- src/components/form-header/index.tsx
+++ src/components/form-header/index.tsx
@@ -4,8 +4,8 @@ export default function Form() {
   return (
     <main className="flex min-h-screen items-start justify-center bg-gray-50 p-6">
       <div className="w-full max-w-2xl rounded-2xl bg-white p-6 shadow">
-        <h1 className="text-brand-700 mb-2 text-center text-2xl font-semibold">
-          Client Onboarding
+        <h1 className="text-brand-700 mb-2 text-center text-2xl font-semibold capitalize">
+          Client Onboarding form
         </h1>
 
         <OnBardingForm />
